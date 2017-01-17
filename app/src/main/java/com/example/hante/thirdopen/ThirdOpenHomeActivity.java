580cad2752ab4d02424e8f2e8826984bc784c1fb
Hante <13051551994@163.com>
2017-01-17 17:05:04 +0800

    first commit

---
+++ app/src/main/java/com/example/hante/thirdopen/ThirdOpenHomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.hante.thirdopen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThirdOpenHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_open_home);
+    }
+}
