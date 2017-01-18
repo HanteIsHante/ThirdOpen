1cdf773908fc65f6852a0f89984929e10e52ec8a
Hante <13051551994@163.com>
2017-01-18 19:46:40 +0800

    添加 butterknife  库

---
+++ app/src/main/java/com/example/hante/thirdopen/ThirdOpenApplication.java
@@ -0,0 +1,16 @@
+package com.example.hante.thirdopen;
+
+import android.app.Application;
+
+/**
+ * Created By HanTe
+ * 全局Application 进行初始化的地方
+ */
+
+public class ThirdOpenApplication extends Application{
+
+    @Override
+    public void onCreate () {
+        super.onCreate();
+    }
+}
