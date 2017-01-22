bdcb89640d93227d6a8f146b042c4bef37eb059f
Hante <13051551994@163.com>
2017-01-22 11:36:43 +0800

    添加 util 包

--- app/src/main/java/com/example/hante/thirdopen/BaseActivity.java
+++ app/src/main/java/com/example/hante/thirdopen/BaseActivity.java
@@ -1,14 +1,17 @@
 package com.example.hante.thirdopen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class ThirdOpenHomeActivity extends AppCompatActivity {
+/**
+ * Created By HanTe
+ */
+
+public class BaseActivity  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third_open_home);
 
     }
 }
