c72af45987216bcbc192206de9662a3de9b78333
Hante <13051551994@163.com>
2017-02-07 11:31:22 +0800

    添加登录页面

--- app/src/main/java/com/example/hante/thirdopen/useractivity/LoginActivity.java
+++ app/src/main/java/com/example/hante/thirdopen/useractivity/LoginActivity.java
@@ -2,14 +2,27 @@ package com.example.hante.thirdopen.useractivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.hante.thirdopen.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.username)
+    EditText mUsername;
+    @BindView(R.id.password)
+    EditText mPassword;
+    @BindView(R.id.button_login)
+    Button mButtonLogin;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 }
